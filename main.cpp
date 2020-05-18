ac966555aabbec8854d888529be2b8925adfd227
Erick Salas-Chaverri <ericksc@gmail.com>
2020-05-17 21:10:19 -0600

    funciona

--- main.cpp
+++ main.cpp
@@ -6,8 +6,9 @@
 #define N 205 //Block size
 #define PI 3.14159265358979
 float coeff;
-float Q1;
-float Q2;
+int fix_coeff;
+int Q1;
+int Q2;
 double sine;
 double cosine;
 SAMPLE testData[N];
@@ -15,31 +16,6 @@ SAMPLE testData[N];
 //Para definir punto fijo de 32 bits.
 int FIXED_POINT = 8;
 int ONE = 1 << FIXED_POINT;
-
-/* Call this routine before every "block" (size=N) of samples. */
-void ResetGoertzel(void)
-{
-    Q2 = 0;
-    Q1 = 0;
-}
-/* Call this once, to precompute the constants. */
-void InitGoertzel(void)
-{
-    int k;
-    double floatN;
-    double omega;
-    floatN = (double)N;
-    k = (int)(0.5 + ((floatN * TARGET_FREQUENCY) / SAMPLING_RATE));
-    omega = (2.0 * PI * k) / floatN;
-    sine = sin(omega);
-    cosine = cos(omega);
-    coeff = 2.0 * cosine;
-    printf("For SAMPLING_RATE = %f", SAMPLING_RATE);
-    printf(" N = %d", N);
-    printf(" and FREQUENCY = %f,\n", TARGET_FREQUENCY);
-    printf("k = %d and coeff = %f\n\n", k, coeff);
-    ResetGoertzel();
-}
 
 int toFix( float val ) {
     // Escalamiento
@@ -60,12 +36,38 @@ int mul(int a, int b) {
     return (int) ((long)a * (long)b )>> FIXED_POINT;
 }
 
+/* Call this routine before every "block" (size=N) of samples. */
+void ResetGoertzel(void)
+{
+    Q2 = 0;
+    Q1 = 0;
+}
+/* Call this once, to precompute the constants. */
+void InitGoertzel(void)
+{
+    int k;
+    double floatN;
+    double omega;
+    floatN = (double)N;
+    k = (int)(0.5 + ((floatN * TARGET_FREQUENCY) / SAMPLING_RATE));
+    omega = (2.0 * PI * k) / floatN;
+    sine = sin(omega);
+    cosine = cos(omega);
+    coeff = 2.0 * cosine;
+    fix_coeff = toFix(coeff);
+    printf("For SAMPLING_RATE = %f", SAMPLING_RATE);
+    printf(" N = %d", N);
+    printf(" and FREQUENCY = %f,\n", TARGET_FREQUENCY);
+    printf("k = %d and coeff = %f\n\n", k, coeff);
+    ResetGoertzel();
+}
+
 /* Call this routine for every sample. */
 void ProcessSample(SAMPLE sample)
 {
     // Punto fijo 32. INT
-    float Q0;
-    Q0 = floatVal(mul(toFix(coeff), toFix(Q1)) - toFix(Q2) + toFix(sample));
+    int Q0;
+    Q0 = mul(fix_coeff, Q1) - Q2 + toFix(sample);
     Q2 = Q1;
     Q1 = Q0;
 }
@@ -75,7 +77,7 @@ void ProcessSample(SAMPLE sample)
 float GetMagnitudeSquared(void)
 {
     float result;
-    result = Q1 * Q1 + Q2 * Q2 - Q1 * Q2 * coeff;
+    result = floatVal(Q1) * floatVal(Q1) + floatVal(Q2) * floatVal(Q2) - floatVal(Q1) * floatVal(Q2) * coeff;
     return result;
 }
 /*** End of Goertzel-specific code, the remainder is test code. */
